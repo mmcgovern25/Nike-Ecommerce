a98458978195cc9c079dd65dcde377d7e2b4c123
Matthew McGovern <mattmcg37@gmail.com>
2024-02-13 16:04:29 -0300

    getting the party started

---
+++ Nike-Store/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <h1 className='bg-black text-white text-4xl'>Welcome to Nike Store</h1>
+      <p>Nice to meet you!!!</p>
+    </>
+  )
+}
+
+export default App
