dd4ec34c07d1ffd0e94f5f1cbb8208aae2e41c15
Matthew McGovern <mattmcg37@gmail.com>
2024-02-14 23:11:10 -0300

    were rolling baby

--- Nike-Store/src/App.jsx
+++ Nike-Store/src/App.jsx
@@ -1,4 +1,4 @@
-import { Hero } from '.components';
+import { Hero } from './components';
 import { heroapi } from './data/data';
 
 const App = () => {
--- Nike-Store/src/components/Hero.jsx
+++ Nike-Store/src/components/Hero.jsx
@@ -3,8 +3,8 @@
 const Hero = ({ heroapi: {title, subtitle, btntext, img, sociallinks, videos} }) => {
   return (
     <div className='relative h-auto w-auto flex flex-col' >
-      <div className='bg-theme clip-path h-[85vh] md:h-[65vh] sm:h[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10' ></div>
-      <div className='' >
+      <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10' ></div>
+      <div className='relative opacity-100 z-20' >
         <div className='' >
             <h1>{title}</h1>
             <h1>{subtitle}</h1>
@@ -14,7 +14,7 @@ const Hero = ({ heroapi: {title, subtitle, btntext, img, sociallinks, videos} })
         </div>
         <div >
           <img
-            src='{img}'
+            src={img}
             alt='hero-img/img'
             className=''
           />
