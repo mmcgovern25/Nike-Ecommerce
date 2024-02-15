9d4943b3003fc0f6fb2b5ceb5b88baa15ff2b075
Matthew McGovern <mattmcg37@gmail.com>
2024-02-15 18:32:42 -0300

    video icons

--- Nike-Store/src/components/utils/Clips.jsx
+++ Nike-Store/src/components/utils/Clips.jsx
@@ -1,14 +1,17 @@
-
+import { PlayIcon } from "@heroicons/react/24/solid";
 
 const Clips = ({ clip, imgsrc }) => {
   return (
     <>
-      <div className='relative h-28 w-32 rounded-xl overflow-hidden group cursor-pointer transition-all duration-300 '>
+      <div className='relative h-28 w-32 rounded-xl overflow-hidden group cursor-pointer transition-all duration-300 lg:w-28 md:w-24 sm:w-16 lg:h-24 md:h-20 sm:h-14 '>
         <img
           src={imgsrc}
           alt="img/clips"
           className='inset-0 flex h-full w-full object-cover absolute top-0 left-0 right-0 rounded-xl opacity-100 z-10 transition-opacity duration-500'
         />
+        <div className='bg-white blue-effect-theme absolute top-0 left-0 right-0 opacity-100 z-[100]'>
+          <PlayIcon className='icon-style text-slate-900'/>
+        </div>
         <video
           autoPlay={true}
           loop={true}
