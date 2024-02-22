a05c61f23f3f0420d723d7f50ec4e665758d37a0
Matthew McGovern <mattmcg37@gmail.com>
2024-02-22 17:25:37 -0300

    redux start

---
+++ Nike-Store/src/app/CartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  cartState: false,
+
+};
+
+const CartSlice = createSlice({
+  initialState,
+  name: "cart",
+  reducers: {
+    setOpenCart: (state, action) => {
+      state.cartState = action.payload.cartState;
+    },
+    setCloseCart: (state, action ) => {
+      state.cartState = action.payload.cartState;
+    }
+  }
+});
+
+export const { setOpenCart, setCloseCart } = CartSlice.actions;
+
+export default CartSlice.reducer;
